e573f1ea7208fcf25104b5e8313b82a268a1e013
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-08-09 12:44:11 +0530

    https://dealmart-online.netlify.app

--- src/main/java/com/dealmart/ecommerce/controller/ProductController.java
+++ src/main/java/com/dealmart/ecommerce/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import com.dealmart.ecommerce.model.Product;
 import com.dealmart.ecommerce.service.ProductService;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 public class ProductController {
 
