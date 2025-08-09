a2eeeecdcfef91442f2a4fc4b6b181ecd5984b66
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-08-09 12:48:08 +0530

    @CrossOrigin(origins = https://dealmart-online.netlify.app)

--- src/main/java/com/dealmart/ecommerce/controller/ProductController.java
+++ src/main/java/com/dealmart/ecommerce/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.dealmart.ecommerce.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import com.dealmart.ecommerce.model.Product;
 import com.dealmart.ecommerce.service.ProductService;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://dealmart-online.netlify.app")
 @RestController
 public class ProductController {
 
@@ -19,21 +18,20 @@ public class ProductController {
     public ResponseEntity<?> addProduct(@RequestBody Product product) {
         return productService.addProduct(product);
     }
-    
+
     @GetMapping("/product")
     public ResponseEntity<?> findAllProduct() {
         return productService.findAllProduct();
     }
-    
+
     @GetMapping("/product/{category}")
-    public ResponseEntity<?> filterCategory(@PathVariable String category){
-    	return productService.filterCategory(category);
+    public ResponseEntity<?> filterCategory(@PathVariable String category) {
+        return productService.filterCategory(category);
     }
- 
+
     @GetMapping("/product/name/{searchData}")
-    public ResponseEntity<?> search(@PathVariable String searchData){
-    	return productService.search(searchData);
+    public ResponseEntity<?> search(@PathVariable String searchData) {
+        return productService.search(searchData);
     }
-  
-    
+
 }
